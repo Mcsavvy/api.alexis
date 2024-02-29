c0db4383e6850952476daec9d3fbdeabe9a74453
John David <davemcsavvii@gmail.com>
2024-02-29 20:50:57 +0000

    refactor: Update get_token and is_authenticated to be async

--- alexis/components/auth.py
+++ alexis/components/auth.py
@@ -9,8 +9,15 @@ from alexis.models import User
 security = HTTPBearer()
 
 
-def get_token(creds: HTTPAuthorizationCredentials = Depends(security)) -> str:
+async def get_token(
+    creds: HTTPAuthorizationCredentials | None = Depends(security),
+) -> str:
     """Get token from header."""
+    if not creds:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+        )
     if not creds.scheme.lower() == "bearer":
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -19,7 +26,7 @@ def get_token(creds: HTTPAuthorizationCredentials = Depends(security)) -> str:
     return creds.credentials
 
 
-def is_authenticated(token: str = Depends(get_token)) -> "User":
+async def is_authenticated(token: str = Depends(get_token)) -> "User":
     """Check if user is authenticated."""
     user = User.get_by_token(token)
     if not user:
