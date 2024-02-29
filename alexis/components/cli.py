5b50031410908fdaa32d71ec931077e73107a54b
John David <davemcsavvii@gmail.com>
2024-02-29 18:57:18 +0000

    feat(cli): Added shell command

--- alexis/components/cli.py
+++ alexis/components/cli.py
@@ -8,6 +8,38 @@ def cli():
     pass
 
 
+@cli.command(name="shell")
+def shell():
+    """Start a IPython shell with the app context."""
+    from IPython.terminal.embed import InteractiveShellEmbed
+
+    from alexis.app import create_app
+    from alexis.components import redis
+    from alexis.components.database import db
+    from alexis.config import settings
+    from alexis.logging import get_logger
+    from alexis.models import Chat, ChatType, Thread, User
+
+    app = create_app()
+    logger = get_logger()
+
+    shell = InteractiveShellEmbed()
+    with db.scoped_session() as session:
+        local_ns = {
+            "app": app,
+            "db": db,
+            "session": session,
+            "User": User,
+            "Chat": Chat,
+            "ChatType": ChatType,
+            "Thread": Thread,
+            "redis": redis,
+            "settings": settings,
+            "logger": logger,
+        }
+        shell(local_ns=local_ns)
+
+
 @cli.group(name="db")
 def db():
     """Database commands."""
@@ -18,6 +50,7 @@ def db():
 def create_all():
     """Create all tables."""
     from alexis.auth import models  # noqa
+    from alexis.chat import models  # noqa
     from alexis.components.database import db
 
     db.create_all()
@@ -28,6 +61,7 @@ def create_all():
 def drop_all():
     """Drop all tables."""
     from alexis.auth import models  # noqa
+    from alexis.chat import models  # noqa
     from alexis.components.database import db
 
     db.drop_all()
