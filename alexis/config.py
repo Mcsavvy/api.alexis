c8425cafa4e613080f0da0adc86b0da9450fc756
John David <davemcsavvii@gmail.com>
2024-02-29 18:55:23 +0000

    chore: Export OpenAI key to env

--- alexis/config.py
+++ alexis/config.py
@@ -1,6 +1,17 @@
 """Dynaconf configuration file."""
 
+import os
+
+from dotenv import load_dotenv
 from dynaconf import Dynaconf  # type: ignore[import-untyped]
+
+load_dotenv()
+
+
+def export_openai_key(settings: Dynaconf):
+    """Export the OpenAI key."""
+    os.environ["OPENAI_API_KEY"] = settings.openai_api_key
+
 
 settings = Dynaconf(
     envvar_prefix="ALEXIS",
@@ -8,4 +19,5 @@ settings = Dynaconf(
     environments=True,
     load_dotenv=True,
     env_switcher="ALEXIS_ENV",
+    post_hooks=[export_openai_key],
 )
