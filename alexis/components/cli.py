de8054e121adab80c5a449ea29f99be5a42765b6
John David <davemcsavvii@gmail.com>
2024-04-10 20:33:27 +0000

    chore: Remove unused code related to sql

--- alexis/components/cli.py
+++ alexis/components/cli.py
@@ -31,8 +31,6 @@ def shell():
     shell = InteractiveShellEmbed()
     local_ns = {
         "app": app,
-        "db": db,
-        "session": session,
         "User": User,
         "Chat": Chat,
         "ChatType": ChatType,
@@ -42,31 +40,3 @@ def shell():
         "logger": logger,
     }
     shell(local_ns=local_ns)
-
-
-@cli.group(name="db")
-def db():
-    """Database commands."""
-    pass
-
-
-@db.command(name="create-all")
-def create_all():
-    """Create all tables."""
-    from alexis.auth import models  # noqa
-    from alexis.chat import models  # noqa
-    from alexis.components.database import db
-
-    db.create_all()
-    click.echo("All tables created.")
-
-
-@db.command(name="drop-all")
-def drop_all():
-    """Drop all tables."""
-    from alexis.auth import models  # noqa
-    from alexis.chat import models  # noqa
-    from alexis.components.database import db
-
-    db.drop_all()
-    click.echo("All tables dropped.")
